955a1a22bd2e8bb8447075ce3f8ce1915b7d3859
Francesco Malaspina <franci.malaspina@gmail.com>
2023-09-08 17:27:28 +0200

    update homepage frontend

--- pages/home.py
+++ pages/home.py
@@ -28,13 +28,18 @@ def spot_price(data=data, day=None):
         variation = price - prev_price
         percentage_variation = variation / prev_price
 
-    html_text = html.H4([f"{price:.2f} USD",
-                         html.Span(
-                             f"{variation:.2f} ({percentage_variation:.4f}%)",
-                             style={
-                                 "color": "red" if percentage_variation < 0 else "green",
-                                 "font-size": "18px",
-                                 "margin-left": "10px"})])
+    html_text = html.H4([
+        f"{price:.2f} USD",
+        html.Br(),  # add this line
+        html.Span(
+            f"{variation:.2f} ({percentage_variation:.4f}%)",
+            style={
+                "color": "red" if percentage_variation < 0 else "green",
+                "font-size": "18px",
+                "margin-left": "10px"
+            }
+        )
+    ])
     return html_text
 
 
@@ -49,56 +54,78 @@ date_picker = dbc.InputGroup([
     )
 ])
 
-layout = dbc.Container([
+col1 = dbc.Col([
+    html.Br(),
+    html.Br(),
+    html.Br(),
+    # html.H5("Spot price"),
     # html.Br(),
-    # dbc.Row("Welcome to the homepage!"),
+    dbc.Card([dbc.CardHeader("Spot price"),
+              dbc.CardBody(spot_price(), id="BTC-price-card"),]),
+    html.Br(),
+    date_picker,
+    html.Br(),
+    html.H5("Graph options"),
+    # html.Br(),
+    dbc.InputGroup([
+        dbc.InputGroupText("Scale"),
+        dbc.Select(
+            id='y-axis-scale',
+            options=[{'label': 'Linear', 'value': 'linear'},
+                     {'label': 'Logarithmic', 'value': 'log'}],
+            value='linear',
+        ),
+    ]),
+    html.Br(),
+    dbc.InputGroup([
+        dbc.Select(
+            id='x-axis-range',
+            options=[
+                {'label': '1W', 'value': -7},
+                {'label': '1M', 'value': -30},
+                {'label': '6M', 'value': -180},
+                {'label': 'YTD', 'value': 'YTD'},
+                {'label': '1Y', 'value': -365},
+                {'label': 'All time', 'value': 'Max'}],
+            value=-30,
+        ), dbc.InputGroupText("Range"),
+    ]),
+], width={"size": 3})
+
+col2 = dbc.Col([
+    html.Br(),
+    html.H1("Historical price", style={"text-align": "right"}),
+    dcc.Graph(
+                id='candlestick-price-chart',
+                config={'staticPlot': False},
+                figure={}
+    )
+])
+
+layout = dbc.Container([
     html.Br(),
     dbc.Row([
-        dbc.Col([date_picker], width={"size": 3, "offset": 0.5}),
-        dbc.Col(dbc.Card(
-            dbc.CardBody(spot_price(),
-                         id="BTC-price-card"),
-        ),width={"size": 5, "offset": -1}),
-    ], align="center"),
-    html.Br(),
-    html.H2("Historical price"),
-    html.Br(),
-    dbc.Row(
-        [
-            dbc.Col("Select price scale:", width="auto"),
-            dbc.Col(
-                dcc.Dropdown(
-                    id='y-axis-scale',
-                    options=[{'label': 'Linear scale', 'value': 'linear'},
-                             {'label': 'Logarithmic scale', 'value': 'log'}],
-                    value='linear',
-                    clearable=False)),
-            dbc.Col("Select period range:", width="auto"),
-            dbc.Col(
-                dcc.Dropdown(
-                    id='x-axis-range',
-                    options=[
-                        {'label': '1W', 'value': -7},
-                        {'label': '1M', 'value': -30},
-                        {'label': '6M', 'value': -180},
-                        {'label': 'YTD', 'value': 'YTD'},
-                        {'label': '1Y', 'value': -365},
-                        {'label': 'All time', 'value': 'Max'}],
-                    value=-30))]),
-    dcc.Graph(
-        id='candlestick-price-chart',
-        config={'staticPlot': False},
-        figure={}),
+        col1,
+        col2,
+    ]),
 ], fluid=False)
+
+
+@callback(
+    Output("BTC-price-card", "children"),
+    Input("my-date-picker-single", "date")
+)
+def update_card_body(day):
+    return spot_price(data, day)
 
 
 @callback(
     Output('candlestick-price-chart', 'figure'),
     [Input('y-axis-scale', 'value'),
-     Input('x-axis-range', 'value')]
+     Input('x-axis-range', 'value'),
+     Input("my-date-picker-single", "date")]
 )
-def update_figure(y_scale, x_range):
-    # data["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
+def update_figure(y_scale, x_range, selected_date):
     if y_scale == 'log':
         yaxis_type = 'log'
     else:
@@ -107,8 +134,9 @@ def update_figure(y_scale, x_range):
     if x_range == 'YTD':
         _data = data[data["Date"].dt.year == data.iloc[-1, 0].year]
     elif x_range == 'Max':
-        pass
+        _data = data
     else:
+        x_range = int(x_range)
         _data = data[x_range:]
 
     updated_figure = go.Figure(data=[go.Candlestick(
@@ -118,15 +146,19 @@ def update_figure(y_scale, x_range):
         high=_data["High"],
         low=_data["Low"]
     )])
-    updated_figure.update_layout(xaxis_rangeslider_visible=False, title_text="Candlestick price chart")
+
+    updated_figure.update_layout(
+        xaxis_rangeslider_visible=False,
+        title_text="Candlestick price chart",
+        shapes=[dict(
+            type="rect",
+            yref="paper", y0=0, y1=0.1,
+            xref="x", x0=selected_date, x1=selected_date,
+            line=dict(color="blue", width=5)),
+        ]
+    )
+
     updated_figure.update_xaxes(title_text="date")
     updated_figure.update_yaxes(type=yaxis_type, title_text="price")
+
     return updated_figure
-
-
-@callback(
-    Output("BTC-price-card", "children"),
-    Input("my-date-picker-single", "date")
-)
-def update_card_body(day):
-    return spot_price(data, day)
